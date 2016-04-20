10de7257f340110e9821a4696f8e19bcbccff7ca
Brad Pizzimenti and David Remington <david.ian.remington@gmail.com>
2016-04-20 13:41:35 -0700

    populate letters array

--- app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
@@ -4,18 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class PlayActivity extends AppCompatActivity {
 
-    private String[] consonants = new String[] {"b", "c", "d", "f", "g", "h"};
+    private String[] consonants = new String[] {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "y", "z"};
     private String[] vowels = new String [] {"a", "e", "i", "o", "u"};
-    private String word;
-    public static final String TAG = MainActivity.class.getSimpleName();
+    private ArrayList<String> letters = new ArrayList<String>();
+    private int vowelQty;
+    public static final String TAG = PlayActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        Log.d(TAG, vowels.length);
-        word = vowels[(int) (Math.random() * vowels.length)] + vowels[(int) (Math.random() * vowels.length)] + consonants[(int) (Math.random() * consonants.length)];
+        vowelQty = ((int) (Math.random() * 3 ) + 2);
+        Log.d(TAG, vowelQty + "");
+
+        for(int i = 0; i < vowelQty; i ++ ) {
+            letters.add(vowels[(int) (Math.random() * vowels.length)]);
+        }
+
+        for(int i = 0; i < 8 - vowelQty; i ++) {
+            letters.add(consonants[(int) (Math.random() * consonants.length)]);
+        }
+
+        Log.v("letterLog", letters.toString());
+//        Log.d(TAG, letters);
+
+//        letters = vowels[(int) (Math.random() * vowels.length)] + vowels[(int) (Math.random() * vowels.length)] + consonants[(int) (Math.random() * consonants.length)];
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
