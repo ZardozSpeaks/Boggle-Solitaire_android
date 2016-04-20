cd08c855abaa04dd9f72c0518e9760fd270a2a4a
Brad Pizzimenti and David Remington <brad@gennakersystems.com>
2016-04-20 14:07:50 -0700

    display letter array

--- app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
@@ -3,13 +3,16 @@ package com.epicodus.bogglesolitaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.widget.TextView;
 import java.util.ArrayList;
+
 
 public class PlayActivity extends AppCompatActivity {
 
-    private String[] consonants = new String[] {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "w", "x", "y", "z"};
-    private String[] vowels = new String [] {"a", "e", "i", "o", "u"};
+    TextView mLetterArray;
+
+    private String[] consonants = new String[] {"b", "c", "c", "c", "c", "c", "d", "d", "d", "d", "d", "d", "f", "f", "f", "f", "g", "g", "g", "h", "h", "h", "h", "h", "j", "k", "l", "l", "l", "l", "l", "m", "m", "m", "m", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "p", "p", "p", "p", "qu", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "s", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "v", "w","w", "x", "y", "y", "y", "z"};
+    private String[] vowels = new String [] {"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "u", "u", "u", "u"};
     private ArrayList<String> letters = new ArrayList<String>();
     private int vowelQty;
     public static final String TAG = PlayActivity.class.getSimpleName();
@@ -17,7 +20,13 @@ public class PlayActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        vowelQty = ((int) (Math.random() * 3 ) + 2);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        mLetterArray = (TextView) findViewById(R.id.letterArray);
+
+
+        vowelQty = ((int) (Math.random() * 2 ) + 2);
         Log.d(TAG, vowelQty + "");
 
         for(int i = 0; i < vowelQty; i ++ ) {
@@ -29,11 +38,12 @@ public class PlayActivity extends AppCompatActivity {
         }
 
         Log.v("letterLog", letters.toString());
+
+        mLetterArray.setText(letters.toString());
+
 //        Log.d(TAG, letters);
 
 //        letters = vowels[(int) (Math.random() * vowels.length)] + vowels[(int) (Math.random() * vowels.length)] + consonants[(int) (Math.random() * consonants.length)];
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play);
     }
 }
