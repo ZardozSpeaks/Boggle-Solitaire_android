3448f416ba8d0bbc026c7de8975cb0069c8da8c5
David Remington <david.ian.remington@gmail.com>
2016-05-31 15:55:52 -0700

    add dictionrary resource and input checking

--- app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
@@ -3,25 +3,57 @@ package com.epicodus.bogglesolitaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
+
+import butterknife.Bind;
 
 
 public class PlayActivity extends AppCompatActivity {
 
-    TextView mLetterArray;
+    @Bind(R.id.letterArray) TextView mLetterArray;
+    @Bind(R.id.inputEditText) EditText mInputEditText;
 
     private String[] consonants = new String[] {"b", "c", "c", "c", "c", "c", "d", "d", "d", "d", "d", "d", "f", "f", "f", "f", "g", "g", "g", "h", "h", "h", "h", "h", "j", "k", "l", "l", "l", "l", "l", "m", "m", "m", "m", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "p", "p", "p", "p", "qu", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "s", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "v", "w","w", "x", "y", "y", "y", "z"};
     private String[] vowels = new String [] {"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "u", "u", "u", "u"};
     private ArrayList<String> letters = new ArrayList<String>();
     private int vowelQty;
+    HashSet<String> dictionary = new HashSet<>();
     public static final String TAG = PlayActivity.class.getSimpleName();
+
+    public void LoadDictionary() {
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(getAssets().open("dict")));
+            String mLine;
+            while((mLine = reader.readLine()) != null) {
+                dictionary.add(mLine);
+            }
+        } catch (IOException e) {
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        LoadDictionary();
 
 //puts letter array contents into textView
 
