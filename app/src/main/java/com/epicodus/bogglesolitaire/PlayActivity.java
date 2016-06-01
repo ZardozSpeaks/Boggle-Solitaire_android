5cccf2d0e37f5e5c105de1047fbe37a58d00fc1b
David Remington <david.ian.remington@gmail.com>
2016-05-31 17:13:17 -0700

    add additional logic to check for empty input and word length

--- app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
+++ app/src/main/java/com/epicodus/bogglesolitaire/PlayActivity.java
@@ -3,8 +3,11 @@ package com.epicodus.bogglesolitaire;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,13 +16,15 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Bind(R.id.letterArray) TextView mLetterArray;
     @Bind(R.id.inputEditText) EditText mInputEditText;
-
+    @Bind(R.id.addWordButton) Button mAddWordButton;
+    public ArrayList<String> list = new ArrayList<String>();
     private String[] consonants = new String[] {"b", "c", "c", "c", "c", "c", "d", "d", "d", "d", "d", "d", "f", "f", "f", "f", "g", "g", "g", "h", "h", "h", "h", "h", "j", "k", "l", "l", "l", "l", "l", "m", "m", "m", "m", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "n", "p", "p", "p", "p", "qu", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "r", "s", "s", "s", "s", "s", "s", "s", "s", "s", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "t", "v", "w","w", "x", "y", "y", "y", "z"};
     private String[] vowels = new String [] {"a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e",  "e", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "i", "o", "u", "u", "u", "u"};
     private ArrayList<String> letters = new ArrayList<String>();
@@ -50,10 +55,11 @@ public class PlayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        ButterKnife.bind(this);
         LoadDictionary();
+        mAddWordButton.setOnClickListener(this);
 
 //puts letter array contents into textView
 
@@ -87,5 +93,60 @@ public class PlayActivity extends AppCompatActivity {
 
 //        letters = vowels[(int) (Math.random() * vowels.length)] + vowels[(int) (Math.random() * vowels.length)] + consonants[(int) (Math.random() * consonants.length)];
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.addWordButton) {
+            String userInput = mInputEditText.getText().toString();
+            String randomGeneratedString = mLetterArray.getText().toString();
+            char[] randomGeneratedCharArray = randomGeneratedString.toCharArray();
+            String testString = new String();
+
+            if (userInput.length() >= 3) {
+                if (dictionary.contains(userInput)) {
+                    for (int i = 0; i < userInput.length(); i++) {
+
+                        for (int j = 0; j < randomGeneratedCharArray.length; j++) {
+
+                            if (userInput.charAt(i) == randomGeneratedCharArray[j]) {
+                                testString += userInput.charAt(i);
+                                randomGeneratedCharArray[j] = 0;
+                            }
+                        }
+                    }
+                } else {
+                    Toast.makeText(PlayActivity.this, "It's not a word, partner!", Toast.LENGTH_SHORT).show();
+                }
+            }
+            if (!userInput.matches("")) {
+                if (testString.equals(userInput)) {
+                    if (list.size() > 0) {
+                        boolean isRepeat = false;
+                        for (int i = 0; i < list.size(); i++) {
+                            if (list.get(i).equals(userInput)) {
+                                Toast.makeText(PlayActivity.this, "No Repeats!", Toast.LENGTH_SHORT).show();
+                                isRepeat = true;
+                                break;
+                            }
+                        }
+                        if (isRepeat == false) {
+                            list.add(userInput);
+                            Toast.makeText(PlayActivity.this, "Good job!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        list.add(userInput);
+                        Toast.makeText(PlayActivity.this, "Good job!", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(PlayActivity.this, "illegal word!", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(PlayActivity.this, "you must enter a word", Toast.LENGTH_SHORT).show();
+            }
+
+            Log.d(TAG, list.toString());
+            mInputEditText.setText(null);
+        }
     }
 }
